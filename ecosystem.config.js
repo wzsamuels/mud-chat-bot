69c87c6b815e9093c6f6e1335137971bcd1db958
Zach Samuels <wzsamuels@gmail.com>
2025-03-23 16:39:19 -0400

    Add pm2 support

---
+++ ecosystem.config.js
@@ -0,0 +1,15 @@
+export default {
+  apps: [{
+    name: 'mud-bot',
+    script: 'src/bot.js',
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'production'
+    },
+    error_file: 'logs/pm2-error.log',
+    out_file: 'logs/pm2-out.log',
+    log_date_format: 'YYYY-MM-DD HH:mm:ss',
+    merge_logs: true
+  }]
+}; 
