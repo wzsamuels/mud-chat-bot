09e13aa099f3807bfe830e3db5e960eca57c7bb4
Zach Samuels <wzsamuels@gmail.com>
2026-07-17 14:55:27 -0400

    Remove unused files

--- trainMarkov.js
+++
@@ -1,9 +0,0 @@
-import MarkovGenerator from './src/MarkovGenerator.js'
-
-const bot = new MarkovGenerator();
-console.log(bot)
-let text = bot.generate()
-console.log(text)
-
-text = bot.generateReply("Your mother is a whore")
-console.log(text)
