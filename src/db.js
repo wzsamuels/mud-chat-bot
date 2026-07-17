7a2eb6e981f510f10eb259a5260a963eb363a4e1
Zach Samuels <wzsamuels@gmail.com>
2026-07-17 14:47:24 -0400

    Remove unused files

--- src/db.js
+++
@@ -1,7 +0,0 @@
-import Database from 'better-sqlite3';
-import path from 'path';
-
-// Open the database strictly in read-only mode
-const db = new Database(path.join(process.cwd(), 'markov.db'), { readonly: true });
-
-export default db;
