829b5a8c2de98b05be1049b6bce185091ef94eca
Zach Samuels <wzsamuels@gmail.com>
2026-07-17 15:40:49 -0400

    Clean up pm2 config file

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,14 +2,10 @@ module.exports = {
   apps: [{
     name: 'mud-bot',
     script: 'main.js',
-    watch: false,
     max_memory_restart: '1G',
     env: {
       NODE_ENV: 'production'
     },
-    error_file: 'logs/pm2-error.log',
-    out_file: 'logs/pm2-out.log',
-    log_date_format: 'YYYY-MM-DD HH:mm:ss',
     merge_logs: true
   }]
 };
